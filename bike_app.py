567f3e2fde092d483a7e42e9f1672ebd931b6605
aalopez76 <aalpzp@gmail.com>
2024-05-17 22:15:55 -0600

    release version 1.0

---
+++ bike_app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+from google.cloud import firestore
+from google.oauth2 import service_account
+import json
+
+key_dict = json.loads(st.secrets["textkey"])
+creds = service_account.Credentials.from_service_account_info(key_dict)
+db = firestore.Client(credentials=creds, project="bikes-94735")
+
+
+dbPRODUCTS = db.collection('bikes')
+
+st.header('Nuevo registro')
